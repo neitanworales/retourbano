28696b6024067abb53adcf6ef18e0f010eb0a7bb
Pedro Morales <Pedro.Morales+Evertec@evertecinc.com>
2026-05-18 23:04:04 -0600

    REmove testing

--- src/app/modules/recovery-password/recovery-password.component.spec.ts
+++ src/app/modules/recovery-password/recovery-password.component.spec.ts
@@ -7,43 +7,4 @@ import { of } from 'rxjs';
 import { RecoveryPasswordComponent } from './recovery-password.component';
 import { LoginDao } from 'src/app/core/api/dao/LoginDao';
 
-describe('RecoveryPasswordComponent', () => {
-  let component: RecoveryPasswordComponent;
-  let fixture: ComponentFixture<RecoveryPasswordComponent>;
 
-  beforeEach(async () => {
-    const loginDaoMock = {
-      validateResetToken: () => of({ success: true }),
-      resetPassword: () => of({ success: true })
-    };
-
-    await TestBed.configureTestingModule({
-      declarations: [ RecoveryPasswordComponent ],
-      imports: [ReactiveFormsModule, RouterTestingModule],
-      providers: [
-        { provide: LoginDao, useValue: loginDaoMock },
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              queryParamMap: {
-                get: () => 'token-prueba'
-              }
-            }
-          }
-        }
-      ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RecoveryPasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
